ff45e7175d0e656aa29d779d5e71edb4e34806ac
lalla <juanlalla@gmail.com>
2017-10-20 19:50:51 -0300

    Primer subida de proyecto.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        /// <summary>
+        /// Muestra el menú de la aplicación.
+        /// </summary>
+        static void MostrarMenu()
+        {
+            Console.WriteLine("Qué querés hacer?");
+            Console.WriteLine();
+            Console.WriteLine("1- Sumar dos números");
+            Console.WriteLine("2- Multiplicar dos números");
+            Console.WriteLine("0- Salir");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la calculadora");
+            MostrarMenu();
+
+            int opcion = int.Parse(Console.ReadLine());
+            switch(opcion)
+            {
+                case 1:
+                    //sumar TODO
+                    break;
+                case 2:         
+                    //multiplicar TODO           
+                    break;
+                default:
+                    Console.WriteLine("Opción inválida");                    
+                    MostrarMenu();
+                    break;            
+            }
+
+        }
+    }
+}
